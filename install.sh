a30ddb42bf840eacc408a7d3188653256d434e36
jamssismy <126220036+jamssismy@users.noreply.github.com>
2025-12-17 15:44:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-# 创建目录
+
+# --- 1. 创建 EmulationStation 配置目录 ---
 mkdir -p /userdata/system/configs/emulationstation/
 
-# 下载自定义菜单文件
-curl -L https://raw.githubusercontent.com/jamssismy/gamestore/main/es_systems_gamestore.cfg -o /userdata/system/configs/emulationstation/es_systems_gamestore.cfg
+# --- 2. 下载自定义菜单文件 ---
+curl -L https://raw.githubusercontent.com/jamssismy/gamestore/main/es_systems_gamestore.cfg \
+     -o /userdata/system/configs/emulationstation/es_systems_gamestore.cfg
 
-echo "自定义菜单安装完成！请重启 EmulationStation 生效。"
+# --- 3. 创建 gamestore ROM 文件夹 ---
+mkdir -p /userdata/roms/gamestore/
+
+echo "自定义菜单安装完成！"
+echo "gamestore 文件夹已创建，可放置游戏资源。"
+echo "请重启 EmulationStation 生效。"
 
